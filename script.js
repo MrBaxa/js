3c4be0530fd8ac4cef6a18e2677004181d253fc5
MrBaxa <maripovbahti@gmail.com>
2025-02-07 16:14:35 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let name = prompt("Ismingizni kiriting");
+let yosh = prompt("yoshinigzni kiriting");
+let mak = prompt("maktabinizni yozing")
+let id = prompt("idni yozing")
+let code = prompt("codning yozing")
+let adres = prompt("adresinizning yozing")
+let sinf = prompt("sinfingizni  yozing")
+let fan =prompt("yoqtirgan faningizni kiriting")
+
+console.log( name );
+console.log( yosh);
+console.log(mak);
+console.log(id);
+console.log(code);
+console.log(adres);
+console.log(sinf);
